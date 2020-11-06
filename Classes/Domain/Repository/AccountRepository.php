92e0e74076a62f1cf6118da29a47bdeeee2f3b77
Timon Kreis <mail@timonkreis.de>
2020-11-06 17:00:18 +0100

    License updated

--- Classes/Domain/Repository/AccountRepository.php
+++ Classes/Domain/Repository/AccountRepository.php
@@ -2,7 +2,7 @@
 /**
  * @author Timon Kreis <mail@timonkreis.de>
  * @copyright by Timon Kreis - All rights reserved
- * @license http://www.opensource.org/licenses/mit-license.html
+ * @license https://www.gnu.org/licenses/gpl-3.0.de.html
  */
 declare(strict_types = 1);
 
