09ba7960e406a0cbb29f9fa9cdccdadd5be3c8f8
Timon Kreis <mail@timonkreis.de>
2020-11-06 23:18:07 +0100

    PageTS added to limit the possible entry types to accounts and packages

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Timon Kreis <mail@timonkreis.de>
+ * @copyright by Timon Kreis - All rights reserved
+ * @license https://www.gnu.org/licenses/gpl-3.0.de.html
+ */
+declare(strict_types = 1);
+
+defined('TYPO3_MODE') || die();
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'tk_composer',
+    'Configuration/TsConfig/Page/account.typoscript',
+    'PageTS to allow only account entries'
+);
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'tk_composer',
+    'Configuration/TsConfig/Page/package.typoscript',
+    'PageTS to allow only package entries'
+);
