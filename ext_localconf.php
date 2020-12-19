6a4640f5c4007cf05d1325401ac5a7aa5713ecf4
Timon Kreis <mail@timonkreis.de>
2020-12-19 15:01:00 +0100

    JavaScript for password element fixed and refactored as RequireJS module

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,7 @@
 ## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
 $_EXTKEY = 'tk_composer';
 
+// Register password wizard
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1602960741] = [
     'nodeName' => 'passwordWizard',
     'priority' => 50,
